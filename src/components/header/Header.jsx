cb72e73804e8ecf3c2ce6ceddc922df72cd822f8
Mahendra Chauhan <mikesch803@gmail.com>
2022-05-30 01:28:31 +0530

    feat : add profile page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,29 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useAuth } from "../../context";
+import { UserIcon } from "../../icons/Icons";
 import "./Header.css";
 export function Header() {
-  const { logoutHandler} = useAuth();
   return (
-      <div className="navbar">
-        <h1 className="navbar-title">
-          <Link to="/" >
-            Fit Video
+    <div className="navbar">
+      <h1 className="navbar-title">
+        <Link to="/">Fit Video</Link>
+      </h1>
+      <div className="navbar-icons">
+        <button className="btn btn-link navbar-login">
+          <Link to="/profile" className="btn btn-link navbar-login">
+            <UserIcon />
           </Link>
-        </h1>
-        <div className="navbar-icons">
-        { localStorage.token ?  
-        <button onClick={logoutHandler} className="btn btn-link navbar-login">
-        logout
-      </button>
-        :<Link to="/login" className="btn btn-link navbar-login">
-            login
-          </Link> 
-           }
-          <Link to="/trend" className="btn btn-link navbar-shop">
-            Trend
-          </Link>          
-        </div>
+        </button>
       </div>
+    </div>
   );
 }
